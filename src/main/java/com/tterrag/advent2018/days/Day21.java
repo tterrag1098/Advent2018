671cbc6e93dc2c71091435915d1aec9045db94a5
tterrag <tterrag1098@gmail.com>
2018-12-22 05:11:11 -0500

    Day 22 - Part 2 not complete

--- src/main/java/com/tterrag/advent2018/days/Day21.java
+++ src/main/java/com/tterrag/advent2018/days/Day21.java
@@ -1,7 +1,5 @@
 package com.tterrag.advent2018.days;
 
-import java.util.BitSet;
-
 import com.tterrag.advent2018.util.Day;
 
 public class Day21 extends Day {
@@ -14,8 +12,7 @@ public class Day21 extends Day {
     protected Result doParts() {
         int[] r = new int[6];
         int part1 = 0;
-        BitSet seen = new BitSet();
-        int part2 = 0;
+        int lowest = Integer.MAX_VALUE;
         // #ip 1
         /*0:  */r[5] = 123;
         do {
@@ -25,25 +22,83 @@ public class Day21 extends Day {
         do {
             /*6:  */r[3] = r[5] | (1 << 16);
             /*7:  */r[5] = 521363;
-            do {
+            L8: do {
                 /*8:  */r[4] = r[3] & 0xFF;
                 /*9:  */r[5] += r[4];
                 /*10:  */r[5] = r[5] & 0xFFFFFF;
                 /*11:  */r[5] = r[5] * 65899;
                 /*12:  */r[5] = r[5] & 0xFFFFFF;
-                /*13:  */if (256 > r[3]) { //r[4] = 256 > r[3] ? 1 : 0;
-                    break;
+                /*13:  */if (256 <= r[3]) { //r[4] = 256 > r[3] ? 1 : 0;
+                    /*17:  */r[4] = 0;
+                    L18: do {
+                        /*18:  */r[2] = r[4] + 1;
+                        /*19:  */r[2] *= 256;
+                        /*20:  */if (r[2] > r[3]) { //r[2] = r[2] > r[3] ? 1 : 0;
+                            /*26:  */r[3] = r[4];
+                            /*27:  */continue L8;//goto 8;
+                        } else {
+                            /*24:  */r[4]++;
+                            /*25:  */continue L18;//goto 18;
+                        }
+                    } while (true);
+                } else {
+                    break L8;
                 }
-                r[3] >>= 8;
             } while (true);
+            if (r[5] < lowest) {
+                lowest = r[5];
+                System.out.println(Integer.toBinaryString(lowest));
+            }
             if (part1 == 0) {
                 part1 = r[5];
             }
-            if (seen.get(r[5])) {
-                return new Result(part1, part2);
-            } else {
-                seen.set(r[5]);
-                part2 = r[5];
+        } /*28:  */ while (r[5] != r[0]);//r[4] = r[5] == r[0] ? 1 : 0;
+
+        return new Result(part1, "");
+    }
+    
+    protected Result optimized() {
+        int[] r = new int[6];
+        int part1 = 0;
+        int lowest = Integer.MAX_VALUE;
+        // #ip 1
+        /*0:  */r[5] = 123;
+        do {
+            /*1:  */r[5] = r[5] & 456;
+        } /*2:  */while (r[5] != 72); //r[5] = r[5] == 72 ? 1 : 0;
+        /*5:  */r[5] = 0;
+        do {
+            /*6:  */r[3] = r[5] | (1 << 16);
+            /*7:  */r[5] = 521363;
+            L8: do {
+                /*8:  */r[4] = r[3] & 0xFF;
+                /*9:  */r[5] += r[4];
+                /*10:  */r[5] = r[5] & 0xFFFFFF;
+                /*11:  */r[5] = r[5] * 65899;
+                /*12:  */r[5] = r[5] & 0xFFFFFF;
+                /*13:  */if (256 <= r[3]) { //r[4] = 256 > r[3] ? 1 : 0;
+                    /*17:  */r[4] = 0;
+//                    L18: do {
+//                        /*18:  */r[2] = r[4] + 1;
+//                        /*19:  */r[2] *= 256;
+//                        /*20:  */if (r[2] > r[3]) { //r[2] = r[2] > r[3] ? 1 : 0;
+//                            /*26:  */r[3] = r[4];
+//                            /*27:  */continue L8;//goto 8;
+//                        } else {
+//                            /*24:  */r[4]++;
+//                            /*25:  */continue L18;//goto 18;
+//                        }
+//                    } while (true);
+                } else {
+                    break L8;
+                }
+            } while (true);
+            if (r[5] < lowest) {
+                lowest = r[5];
+                System.out.println(Integer.toBinaryString(lowest));
+            }
+            if (part1 == 0) {
+                part1 = r[5];
             }
         } /*28:  */ while (r[5] != r[0]);//r[4] = r[5] == r[0] ? 1 : 0;
 
---
+++ src/main/java/com/tterrag/advent2018/days/Day22.java
@@ -0,0 +1,215 @@
+package com.tterrag.advent2018.days;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+import com.tterrag.advent2018.util.Day;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.Value;
+
+public class Day22 extends Day {
+
+    public static void main(String[] args) {
+        new Day22().run();
+    }
+    
+    @Value
+    private static class Point {
+        int x, y;
+        
+        Point move(Direction dir) {
+            return new Point(x + dir.x, y + dir.y);
+        }
+    }
+    
+    @RequiredArgsConstructor
+    enum Direction {
+        N(0, -1),
+        E(-1, 0),
+        S(0, 1),
+        W(1, 0);
+        
+        public final int x, y;
+
+        public static Direction[] valid(Point pos) {
+            if (pos.getX() > 0 && pos.getY() > 0) {
+                return values();
+            } else if (pos.getX() > 0) {
+                return new Direction[] { E, S, W };
+            } else if (pos.getY() > 0) {
+                return new Direction[] { N, S, W };
+            } else {
+                return new Direction[] { S, W };
+            }
+        }
+    }
+    
+    enum Tool {
+        CLIMBING_GEAR,
+        TORCH,
+        NONE,
+    }
+    
+    enum Type {
+        ROCKY(Tool.CLIMBING_GEAR, Tool.TORCH),
+        WET(Tool.CLIMBING_GEAR, Tool.NONE),
+        NARROW(Tool.TORCH, Tool.NONE);
+        
+        private final EnumSet<Tool> validTools;
+        
+        private Type(Tool first, Tool... rest) {
+            validTools = EnumSet.of(first, rest);
+        }
+        
+        boolean canUse(Tool tool) {
+            return validTools.contains(tool);
+        }
+        
+        static Type fromErosion(long erosion) {
+            return values()[(int) (erosion % 3L)];
+        }
+    }
+    
+    @Value
+    @ToString(of = {"tool", "pos", "time"})
+    private static class Node {
+        Tool tool;
+        int time;
+        Point pos;
+        List<Node> path = new ArrayList<>();
+        
+        Node move(Direction dir) {
+            Node ret = new Node(tool, time + 1, pos.move(dir));
+            ret.path.addAll(path);
+            ret.path.add(this);
+            return ret;
+        }
+        
+        Node withTool(Tool tool) {
+            Node ret = new Node(tool, time + 7, pos);
+            ret.path.addAll(path);
+            ret.path.add(this);
+            return ret;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Node other = (Node) obj;
+            if (pos == null) {
+                if (other.pos != null)
+                    return false;
+            } else if (!pos.equals(other.pos))
+                return false;
+            if (tool != other.tool)
+                return false;
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((pos == null) ? 0 : pos.hashCode());
+            result = prime * result + ((tool == null) ? 0 : tool.hashCode());
+            return result;
+        }
+    }
+    
+    private final int depth = 7740;
+    private final Point target = new Point(12, 763);
+    
+    private long getErosion(long index) {
+        return (index + depth) % 20183;
+    }
+    
+    private long getErosion(Map<Point, Long> cache, int x, int y) {
+        Point p = new Point(x, y);
+        if (x == 0 && y == 0) {
+            cache.put(p, getErosion(0L));
+        } else if (x == target.getX() && y == target.getY()) {
+            cache.put(p, getErosion(0L));
+        } else if (y == 0) {
+            cache.put(p, getErosion((x * 16807L) % 20183L));
+        } else if (x == 0) {
+            cache.put(p, getErosion((y * 48271L) % 20183L));
+        } else {
+            Long cached = cache.get(p);
+            if (cached == null) {
+                cache.put(p, getErosion((getErosion(cache, p.x - 1, p.y) * getErosion(cache, p.x, p.y - 1)) % 20183L));
+            }
+        }
+        return cache.get(p);
+    }
+    
+    private Type getType(Map<Point, Long> cache, int x, int y) {
+        return Type.fromErosion(getErosion(cache, x, y));
+    }
+
+    @Override
+    protected Result doParts() {
+        Map<Point, Long> erosionCache = new HashMap<>();
+        Map<Point, Type> typeCache = new HashMap<>();
+
+        int risk = 0;
+        
+        for (int y = 0; y < target.getY() + 1; y++) {
+            for (int x = 0; x < target.getX() + 1; x++) {
+                Type type = getType(erosionCache, x, y);
+                risk += type.ordinal();
+                typeCache.put(new Point(x, y), type);
+            }
+        }
+        
+        PriorityQueue<Node> search = new PriorityQueue<>(Comparator.comparingInt(Node::getTime));
+        search.add(new Node(Tool.TORCH, 0, new Point(0, 0)));
+        Set<Node> seen = new HashSet<>();
+        
+        Node endNode = new Node(Tool.TORCH, 0, target);
+        
+        while (!search.isEmpty()) {
+            Node head = search.poll();
+            Type type = typeCache.get(head.getPos());
+            Set<Node> validNext = new LinkedHashSet<>();
+            Direction[] valid = Direction.valid(head.getPos());
+            for (Direction d : valid) {
+                Node next = head.move(d);
+                Type t = typeCache.computeIfAbsent(next.getPos(), p -> getType(erosionCache, p.getX(), p.getY()));
+                if (t.canUse(next.getTool())) {
+                    validNext.add(next);
+                }
+            }
+            if (validNext.size() < valid.length) {
+                for (Tool tool : Tool.values()) {
+                    if (tool != head.getTool() && type.canUse(tool)) {
+                        validNext.add(head.withTool(tool));
+                    }
+                }
+            }
+            validNext.removeIf(seen::contains);
+            if (validNext.contains(endNode)) {
+                return new Result(risk, validNext.stream().filter(n -> n.equals(endNode)).findFirst().get().getTime());
+            }
+//            validNext.removeIf(n -> n.getPos().getX() > target.getX() + 1000 && n.getPos().getY() > target.getY() + 1000);
+            seen.addAll(validNext);
+            search.addAll(validNext);
+        }
+        
+        return null;
+    }
+}
